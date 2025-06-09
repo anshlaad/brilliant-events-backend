666c0e730e5971d0ee34317c2d9993d9f008b4d9
Ansh Laad <163443003+anshlaad@users.noreply.github.com>
2025-06-10 00:59:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const imageRoutes = require("./routes/imageRoutes");
+const imageRoutes = require("imageRoutes");
 const path = require("path");
 const fs = require("fs");
 
